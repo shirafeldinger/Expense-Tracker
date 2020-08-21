57c5d0d1340c5364b185a02953abe008a20f04aa
Shira Feldinger <shirafeldinger@gmail.com>
2020-08-21 18:22:08 +0300

    Updates

---
+++ precache-manifest.09df4c11a64a69d007a9ef9e068266a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa4c6d26b17b95382ec5a7f59614fc72",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "97b5dc643d8d08a56aa3",
+    "url": "/Expense-Tracker/static/css/main.0b8453a7.chunk.css"
+  },
+  {
+    "revision": "5fb30d7f533eb3da14a3",
+    "url": "/Expense-Tracker/static/js/2.2a59da61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker/static/js/2.2a59da61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97b5dc643d8d08a56aa3",
+    "url": "/Expense-Tracker/static/js/main.f7a60a61.chunk.js"
+  },
+  {
+    "revision": "2360b1a9a44445f9391d",
+    "url": "/Expense-Tracker/static/js/runtime-main.8c9bd63d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Expense%20Tracker/precache-manifest.e782d00e893ae5cfe07ed1dd438d4d88.js"
+  "/Expense-Tracker/precache-manifest.09df4c11a64a69d007a9ef9e068266a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Expense%20Tracker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Expense-Tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
